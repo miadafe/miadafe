70dfe98c9647e875424ac4df8a708dedea7f9554
mia dafe <miadafe@mias-MacBook-Air-2.local>
2024-11-10 19:58:43 +0000

    slightly better polaroid clicker

--- src/Components/PhotoClicker.jsx
+++ src/Components/PhotoClicker.jsx
@@ -9,10 +9,12 @@ import iconE from '/Users/miadafe/Documents/web_dev/mckd_web/src/assets/lain.png
 import { useStoreActions, useStoreState } from "easy-peasy";
 import "../../src/index.css"
 
-    const icons = [iconA, iconB, iconC, iconD, iconC];
+    //const icons = [iconA, iconB, iconC, iconD, iconC];
 
 
 export function PhotoClicker(){
+    const polaroids = useStoreState(state => state.polaroids);
+
     let wheelNumber = useStoreState(state => state.wheelNumber);
 
     const changeWheelNum = useStoreActions(action => action.setWheelNumber);
@@ -26,7 +28,7 @@ function incrementwheelNum() {
 
     changeWheelNum(wheelNumber += 1);
 
-    if (wheelNumber > icons.length -1){
+    if (wheelNumber > polaroids.length -1){
         changeWheelNum(0);
         console.log("set to 0:", wheelNumber)
     }
@@ -39,7 +41,7 @@ function decrementwheelNum() {
     changeWheelNum(wheelNumber -= 1);
 
     if (wheelNumber < 0){
-        changeWheelNum(icons.length - 1);
+        changeWheelNum(polaroids.length - 1);
         //console.log("set to 0:", wheelNumber)
     }
     console.log(wheelNumber);
